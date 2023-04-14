86e6dc4cfe87075f302c03eec3ba398b7c211777
Ilias Baali <ilias.baali@student.isae-supaero.fr>
2023-04-14 19:46:36 +0200

    Created the Tournament model

--- tournois/tournaments/admin.py
+++ tournois/tournaments/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Tournoi
 
 # Register your models here.
+admin.site.register(Tournoi) 
--- tournois/tournaments/models.py
+++ tournois/tournaments/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Tournoi(models.Model):
+    name = models.CharField(max_length=200)
+    location = models.CharField(max_length=200, null=True)
+    start_date = models.DateTimeField("Start date", null = True)
+    end_date = models.DateTimeField("End date", null = True)
+    nb_pools = models.IntegerField("Number of pools")
+    nb_teams_in_pool = models.IntegerField("Number of teams per pool")
+    
+    def __str__(self) -> str:
+        return self.name
+    
