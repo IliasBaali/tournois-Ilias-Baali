4bfdbee6c160c4f90c9c3deed5d5528ce830d04e
Ilias Baali <ilias.baali@student.isae-supaero.fr>
2023-04-19 09:13:48 +0200

    Added template heritancy

--- tournois/tournaments/views.py
+++ tournois/tournaments/views.py
@@ -5,21 +5,24 @@ from .models import Tournoi, Poule, Match
 # Create your views here.
 def tournois(request):
     tournois_list = get_list_or_404(Tournoi)
-    if request.user.is_authenticated:
-        user_name = request.user.username
-    else : 
-        user_name = None
-    return render(request, 'tournaments/tournois.html', {'tournois_list': tournois_list, 'username':user_name})
+    return render(request, 'tournaments/tournois.html', context_with_user(request,{'tournois_list': tournois_list}))
 
 def tournoiDetail(request, tournament_id):
     tournament = get_object_or_404(Tournoi, pk=tournament_id)
-    return render(request, 'tournaments/tournoi_detail.html', {'tournoi': tournament})
+    return render(request, 'tournaments/tournoi_detail.html', context_with_user(request,{'tournoi': tournament}))
 
 def pouleDetail(request, pool_id):
     pool = get_object_or_404(Poule, pk=pool_id)
-    return render(request, 'tournaments/poule_detail.html', {'poule': pool})
+    return render(request, 'tournaments/poule_detail.html', context_with_user(request,{'poule': pool}))
 
 def matchDetail(request, match_id):
     match = get_object_or_404(Match, pk=match_id)
-    return render(request, 'tournaments/match_detail.html', {'match': match})
+    return render(request, 'tournaments/match_detail.html', context_with_user(request,{'match': match}))
 
+def context_with_user(request, context):
+    if request.user.is_authenticated:
+        username = request.user.username
+    else : 
+        username = None
+    context['username'] = username
+    return context
