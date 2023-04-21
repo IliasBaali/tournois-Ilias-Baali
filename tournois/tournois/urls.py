ade10b6b4d7c73003cc0194c5281c7d785c83644
Ilias Baali <ilias.baali@student.isae-supaero.fr>
2023-04-21 13:10:49 +0200

    Added empty url

--- tournois/tournois/urls.py
+++ tournois/tournois/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('tournaments.urls')),
     path('admin/', admin.site.urls),
     path('tournois/', include('tournaments.urls')),
     path('accounts/',include('django.contrib.auth.urls'))
